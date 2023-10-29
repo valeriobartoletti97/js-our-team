a00e59c52fa821f6ff1636ba3c010ce5b08d79c2
valeriobartoletti97 <valerio.bartoletti97gmail.com>
2023-10-29 22:20:46 +0100

    team object and basic html created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const team =[
+    {
+        name:'Wayne Barnett',
+        role: 'Founder & CEO',
+        picture: 'img/wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name:'Angela Carroll',
+        role: 'Chief Editor',
+        picture: 'img/angela-caroll-chief-editor.jpg'
+    },
+    {
+        name:'Walter Gordon',
+        role: 'Office Manager',
+        picture: 'img/walter-gordon-office-manager.jpg'
+    },
+    {
+        name:'Angela Lopez',
+        role: 'Social Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name:'Scott Estrada',
+        role: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name:'Barbara Ramos',
+        role: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    },
+];
